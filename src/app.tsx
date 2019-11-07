024f87d107c4b7964f28c0624369bd4c3d12dff7
Quentin Danneville <quentin@nextmap.io>
2019-11-07 14:39:07 +0100

    added react to our project

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+class App extends React.component {
+    render() {
+        return (
+            <h1>Bonjour je suis un titre</h1>
+        )
+    }
+}
+
+// L'export par défault de notre fichier app.tsx est la class App qui a été extends par React
+// Mon app peut être désormais importé dans d'autres fichiers de mon projet
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,27 @@
-alert("Le js s'est bien loadé");
+// alert("Le js s'est bien loadé");
+
+// function hello(word) {
+//     alert(word);
+// }
+
+// function helloTS(word:string) {
+//     alert(word);
+// }
+
+// hello('Salut');
+// helloTS('salut');
+
+// J'importe React
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+//On importe le composant App
+// import App from "./app";
+
+//On stock dans la variable MOUNT_NODE, l'élément HTML dans lequelle on va monter l'application REACT
+const MOUNT_NODE = document.getElementById("app");
+
+//ReactDOM va générer un DOM virtuel à partir de notre MOUNT_NODE
+ReactDOM.render(<h1>Bonjour, je suis un titre</h1>, MOUNT_NODE);
+
+
